7cf49bc80dae1b3ea8e88e4ea1f8bdc8cf9ca044
yakshaG <audit536@gmail.com>
2024-06-30 19:48:00 +0530

    Set up defaults and add assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+    env: {
+        // Sample LINK prefix: /listen. Do not add the slash at the last
+        LINK_PREFIX: isProd ? "/listen" : ""
+    },
+    basePath: isProd ? '/listen' : '',
+    assetPrefix: isProd ? '/listen' : '',
+};
 
 export default nextConfig;
