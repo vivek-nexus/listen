c1d56d6edfb0a7e615b6bba72af7bbdd7cfb9729
vivek-nexus <audit536@gmail.com>
2025-02-15 21:39:14 +0530

    Migrate domain to listen.viveknexus.com

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -1,12 +1,12 @@
-export const APP_HOME_LINK = "https://vivek.nexus/listen"
-export const APP_APP_LINK = "https://vivek.nexus/listen/app"
+export const APP_HOME_LINK = "https://vivek-nexus.github.io/listen"
+export const APP_APP_LINK = "https://vivek-nexus.github.io/listen/app"
 export const GITHUB_REPO_LINK = "https://github.com/vivek-nexus/listen"
 export const GITHUB_ISSUES_LINK = "https://github.com/vivek-nexus/listen/issues"
 // TODO: Update link
 export const GITHUB_MORE_HELP_LINK = "https://github.com/vivek-nexus/listen?tab=readme-ov-file#help"
 // TODO: Update link
 export const GITHUB_INTEGRATE_ON_YOUR_BLOG_LINK = "https://github.com/vivek-nexus/listen?tab=readme-ov-file#listen"
-export const PORTFOLIO_LINK = "https://vivek.nexus"
+export const PORTFOLIO_LINK = "https://vivek-nexus.github.io"
 export const DEFAULT_PASTED_ARTICLE = `Hello! Listen is your world-class multilingual reading companion. You can use it to listen to news pages or blog pages, as if they were podcasts. You can use it to proof-read, or should we say, proof-listen to your writing. You can use it to record voice-overs. So many possibilities, isn't it?
 
 For best listening experience, here are some recommendations. On desktop devices, use Google Chrome or Edge browser for natural sounding voices. On Android and iOS, you may need to first download and install voice data, in your device settings.
@@ -14,7 +14,7 @@ For best listening experience, here are some recommendations. On desktop devices
 If you like Listen, you can install it as an app on your phone or desktop. Even more, you can quickly integrate it on your blog or webpages, so that your readers can instantly listen to your pages.
 
 Okay bye bye!`
-export const CORS_ERROR_MESSAGE = `[LISTEN]: To fetch the article, make sure to allow cross origin requests (CORS) on your server from https://www.vivek.nexus. See https://github.com/vivek-nexus/listen?tab=readme-ov-file#listen.`
+export const CORS_ERROR_MESSAGE = `[LISTEN]: To fetch the article, make sure to allow cross origin requests (CORS) on your server from https://viveknexus.com (NOTE THE DOMAIN NAME CHANGE). See https://github.com/vivek-nexus/listen?tab=readme-ov-file#listen.`
 export const PROXY_SERVER_URL = "https://render-express-server-q222.onrender.com"
 export const SpeechSettingDefaultValues = {
     rate: 10,
